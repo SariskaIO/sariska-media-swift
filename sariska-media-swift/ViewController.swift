1d8efa14a1bf1a89cba46acf87dbef9def8fcf30
dipak140 <dipak@sariska.io>
2023-07-05 13:28:53 +0530

    revert to hardcoded token

--- sariska-media-swift/ViewController.swift
+++ sariska-media-swift/ViewController.swift
@@ -1,17 +1,14 @@
 import UIKit
 import sariska;
 import OSLog;
-import Foundation;
 
 class ViewController: UIViewController {
-	
-    var connection: Connection? = nil
+
+	var connection: Connection? = nil
 	var conference: Conference? = nil
 	var stackView: UIStackView? = nil
 	var localTracks: [JitsiLocalTrack] = []
-    var token: String? = nil
 
-    
 	lazy var videoStackView: UIStackView = {
 		let stackView = UIStackView()
 		stackView.axis = .vertical
@@ -21,85 +18,80 @@ class ViewController: UIViewController {
 		stackView.translatesAutoresizingMaskIntoConstraints = false
 		return stackView
 	}()
-    
+
 	override func viewDidLoad() {
-		
-        super.viewDidLoad()
-        
+		super.viewDidLoad()
 		self.view.addSubview(videoStackView)
-    
-        func getToken(){
-            DispatchQueue.main.async {
-                let tokenReq = tokenRequest()
-                
-                tokenReq.getToken{[weak self] result in
-                                    switch result{
-                                    case .failure(let error):
-                                        print(error)
-                                    case .success(let token2):
-                                        SariskaMediaTransport.initializeSdk()
-                                        
-                                        self?.setupLocalStream()
-                                        
-                                        self?.connection = SariskaMediaTransport.jitsiConnection(token2, roomName: "dipak", isNightly: false)
-                                        
-                                        self?.connection?.addEventListener("CONNECTION_ESTABLISHED", callback: {
-                                            self?.createConference()
-                                        })
-                                        
-                                        self?.connection?.addEventListener("CONNECTION_FAILED", callback: {
-                                        })
-                                        
-                                        self?.connection?.addEventListener("CONNECTION_DISCONNECTED", callback: {
-                                        })
-                                        
-                                        self?.connection?.connect()
-                                    }
-                    }
-            }
-        }
-        
-        getToken()
+
+
+		let tokens = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRkMzc3ZDRjNTBiMDY1ODRmMGY4MDJhYmFiNTIyMjg5ODJiMTk2YzAzNzYwNzE4NDhiNWJlNTczN2JiMWYwYTUiLCJ0eXAiOiJKV1QifQ.eyJjb250ZXh0Ijp7InVzZXIiOnsiaWQiOiJ6cm12aHZidSIsIm5hbWUiOiJmaWVyY2Vfc25haWwifSwiZ3JvdXAiOiIyMDIifSwic3ViIjoicXdmc2Q1N3BxOWR4YWtxcXVxNnNlcSIsInJvb20iOiIqIiwiaWF0IjoxNjg4NTQzMDM5LCJuYmYiOjE2ODg1NDMwMzksImlzcyI6InNhcmlza2EiLCJhdWQiOiJtZWRpYV9tZXNzYWdpbmdfY28tYnJvd3NpbmciLCJleHAiOjE2ODg2Mjk0Mzl9.XSuPg6mfg8h1e4R9f3TG2CLZcbj9n41gy_rOd8kTeiPBpktpOBmwL0yK1BLy-J46XM8WZDyUcwhymCc6PCzxGUmNAQDGyFwxjHuMVhneBOtdAxdooQmxk24W5PXnZtTZWT3AlrZc0ysMG5mOWGmh1KVbTVGMahyui7SeHg9d1DJtPIWLd2RKkg3talkRXhonwsvZ_VzxUN76myfGRw7q_AlvvugyXcnQ7j40Ck-mNtnLvndvT4stEtUNXMwGrsU7tTga-jDIiAPXtInAk6fHWbUwNvTsPKTj4s8a9YdQFZCe68aT3d3eW7BeP-sMo4Ohs1EauN-9T5VlatBzaaCjog"
+
+		SariskaMediaTransport.initializeSdk()
+
+		setupLocalStream()
+
+		self.connection = SariskaMediaTransport.jitsiConnection(tokens, roomName: "dipak", isNightly: false)
+
+		if(self.connection == nil){
+			os_log("connection is nill")
+		}
+
+		self.connection?.addEventListener("CONNECTION_ESTABLISHED", callback: {
+			os_log("Inside the first callback")
+			self.createConference()
+		})
+
+		self.connection?.addEventListener("CONNECTION_FAILED", callback: {
+
+			os_log("Inside the second callback")
+		})
+
+		self.connection?.addEventListener("CONNECTION_DISCONNECTED", callback: {
+			os_log("Inside the third callback")
+		})
+
+		self.connection?.connect()
 	}
-	
+
 	func setupLocalStream() {
 		var options:[String: Any] = [:]
 		options["audio"] = true
 		options["video"] = true
 		options["resolution"] = 240
-        SariskaMediaTransport.createLocalTracks(options) { tracks in
-                    DispatchQueue.main.async {
-                        self.localTracks = tracks as! [JitsiLocalTrack]
-                        for track in self.localTracks {
-                            if (track.getType() == "video")  {
-                                let videoView =  track.render()
-                                self.attachVideo(videoView:  videoView, trackId: (track as AnyObject).getId())
-                            }
-                        }
-                    }
-        }
+		os_log("We are in setup local stream")
+		SariskaMediaTransport.createLocalTracks(options) { tracks in
+			DispatchQueue.main.async {
+				self.localTracks = tracks as! [JitsiLocalTrack]
+				for track in self.localTracks {
+					if (track.getType() == "video")  {
+						let videoView =  track.render()
+						self.attachVideo(videoView:  videoView, trackId: (track as AnyObject).getId())
+					}
+				}
+			}
+		}
 	}
-	
-	
+
+
 	func createConference() {
-		
+
 		guard let connection = connection else {
 			return
 		}
-		
+
 		conference = connection.initJitsiConference()
-		
-        conference?.addEventListener("CONFERENCE_JOINED") {
+
+		conference?.addEventListener("CONFERENCE_JOINED") {
 			for track in self.localTracks {
-                self.conference?.addTrack(track: track)
+				self.conference?.addTrack(track: track)
 			}
 		}
-		
-        conference?.addEventListener("TRACK_ADDED") { track in
+
+		conference?.addEventListener("TRACK_ADDED") { track in
 			let track = track as! JitsiRemoteTrack
-            if(track.getStreamURL() == self.localTracks[1].getStreamURL()){
-                return;
-            }
+			if(track.getStreamURL() == self.localTracks[1].getStreamURL()){
+				return;
+			}
 			DispatchQueue.main.async {
 				if (track.getType() == "video") {
 					let videoView =  track.render()
@@ -107,24 +99,29 @@ class ViewController: UIViewController {
 				}
 			}
 		}
-		
-        conference?.addEventListener("TRACK_REMOVED") { track in
+
+		conference?.addEventListener("TRACK_REMOVED") { track in
 			let track = track as! JitsiRemoteTrack
 			DispatchQueue.main.async {
 				self.removeVideo(trackId: track.getId())
 			}
 		}
-		
-        conference?.addEventListener("CONFERENCE_LEFT") {
+
+		conference?.addEventListener("CONFERENCE_LEFT") {
 			print("CONFERENCE_LEFT")
 		}
-        
-        conference?.join()
-    
-    }
-	
-	
-	
+
+		conference?.join()
+
+		var streamOptions:[String: Any] = [:]
+		streamOptions["streamId"] = "vtpv-yt0u-pbc1-1fjp-5ps5"
+		streamOptions["mode"] = "stream"
+
+		conference?.startRecording(streamOptions)
+	}
+
+
+
 	func removeVideo( trackId: String) {
 		if let first = self.videoStackView.arrangedSubviews.first(where: { $0.accessibilityLabel == trackId }) {
 			UIView.animate(withDuration: 0.3, animations: {
@@ -135,7 +132,7 @@ class ViewController: UIViewController {
 			}
 		}
 	}
-	
+
 	func attachVideo(videoView: RTCVideoView, trackId: String){
 		videoView.setObjectFit("cover")
 		videoView.accessibilityLabel = trackId
@@ -144,11 +141,11 @@ class ViewController: UIViewController {
 		self.videoStackView.addArrangedSubview(videoView)
 		self.view.layoutIfNeeded()
 	}
-	
+
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
 		conference?.leave()
 		connection?.disconnect()
 	}
-	
+
 }

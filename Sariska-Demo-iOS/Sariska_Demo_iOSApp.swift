d795db66ffd922b9dc0225c36dc006e2eb0b244a
dipak140 <dipak@sariska.io>
2023-07-06 13:34:44 +0530

    Initial Commit

---
+++ Sariska-Demo-iOS/Sariska_Demo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sariska_Demo_iOSApp.swift
+//  Sariska-Demo-iOS
+//
+//  Created by Dipak Sisodiya on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Sariska_Demo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
